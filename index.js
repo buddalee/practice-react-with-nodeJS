a1c064915c3fb20df03de63bcbc69e9b41bae039
acekiller <buddalee1stlove@gmail.com>
2017-08-08 11:08:01 +0800

    try to revice var

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const express = require('express');
-const app = express();
+var express = require('express');
+var app = express();
 
 app.get('/', (req, res) => {
   res.send({ hi: 'there' });
 });
 
-const PORT = process.env.PORT || 5000;
+var PORT = process.env.PORT || 5000;
 app.listen(5000);
