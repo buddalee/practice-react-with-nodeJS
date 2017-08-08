aa2bb395023f8e5a9d4e38adc710325d9bc20a61
acekiller <buddalee1stlove@gmail.com>
2017-08-08 16:48:24 +0800

    update response content

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send({ hi: 'there' });
+  res.send({ body: 'hey i m nervious~' });
 });
 
 const PORT = process.env.PORT || 5000;
