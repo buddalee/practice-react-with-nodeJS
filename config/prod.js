19432055a1a1798d1669bd3dd2b9b0d86d002a4d
acekiller <buddalee1stlove@gmail.com>
2017-08-15 17:24:46 +0800

    Added billing and client side app

--- config/prod.js
+++ config/prod.js
@@ -3,4 +3,6 @@ module.exports = {
   googleClientSecret: process.env.GOOGLE_CLIENT_SECRECT,
   mongoURL: process.env.MONGO_URL,
   cookieKey: process.env.COOKIE_KEY,
+  stripePublishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
+  stripeSecretKey: process.env.STRIPE_SECRET_KEY
 };
